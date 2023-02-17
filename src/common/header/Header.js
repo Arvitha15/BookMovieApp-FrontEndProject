b450a05031f8b6aaaa5dbdcd8565af28ecbbbaf8
Arvitha15 <arvithamvs1998@gmail.com>
2023-02-17 19:16:22 +0530

    creating required folders and files

---
+++ src/common/header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(){
+
+    return(
+        <div>
+            <h1>this is header page</h1>
+        </div>
+    )
+
+}
+
+export default Header;
---
+++ src/screens/details/Details.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Details(){
+
+    return(
+        <div>
+            <h1>this is details page</h1>
+        </div>
+    )
+}
+
+export default Details;
