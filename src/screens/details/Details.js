19b3f2f49cfae94ab295d0ce0c071a80fb349207
Arvitha15 <arvithamvs1998@gmail.com>
2023-02-22 00:09:39 +0530

    details page new additions

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from '@material-ui/core/Typography';
 import Header from "../../common/header/Header";
 import './Details.css';
+import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined';
 
-function Details(){
-
-    return(
+function Details() {
+    const [rating, setRating] = useState(0);
+    return (
         <div>
-            <Header/>
+            <Header />
             <Typography id="backBtn">&lt; Back to Home</Typography>
             <div className="remainingPart">
                 <div className="firstPart">first</div>
-                <div className="middlePart">middle</div>
-                <div className="lastPart">last</div>
+                <div className="middlePart">
+                    <Typography variant="headline" component="h2">movie titile</Typography>
+                    <Typography variant="subtitle2" component="h2">Duration:</Typography>
+                    <Typography variant="subtitle2" component="h2">Release Date:</Typography>
+                    <Typography variant="subtitle2" component="h2">Rating:</Typography>
+                    <Typography variant="subtitle2" component="h2" id="plot">Plot:</Typography>
+                    <Typography variant="subtitle2" component="h2" id="trailer">Trailer:</Typography>
+
+                </div>
+                <div className="lastPart">
+                    <div>
+                        <Typography variant="subtitle2" component="h2">Rate this movie:</Typography>
+                        <div className="start-rating">
+                            {[...Array(5)].map((star, index) => {
+                                index += 1;
+                                return (
+                                    <StarBorderOutlinedIcon style={{ cursor: "pointer" }}
+                                        key={index}
+                                        className={index <= rating ? "on" : "off"}
+                                        onClick={() => setRating(index)}
+                                    >
+
+                                    </StarBorderOutlinedIcon>
+                                );
+                            })}
+                        </div>
+
+                    </div>
+
+                    <Typography variant="subtitle2" component="h2">Artists:</Typography>
+                </div>
             </div>
         </div>
     )
